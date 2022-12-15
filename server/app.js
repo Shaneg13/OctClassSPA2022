33499f090a29f6a28bbb9d6eaac3619d450b6db7
Shaneg13 <sgrewell713@gmail.com>
2022-12-14 23:07:41 -0500

    Pushing class SPA Dec 14 2022

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 // 'Import' the Express module instead of http
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const pizzas = require("./routers/pizzas");
 
 dotenv.config();
 
@@ -8,6 +10,15 @@ const PORT = process.env.PORT || 4040; // we use || to provide a default value
 
 // Initialize the Express application
 const app = express();
+
+mongoose.connect(process.env.MONGODB);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
 
 const logging = (request, response, next) => {
   console.log(`${request.method} ${request.url} ${Date.now()}`);
@@ -29,21 +40,23 @@ app.get("/status", (request, response) => {
   response.status(200).json({ message: "Service healthy" });
 });
 
-app.get("/users/:id", (request, response) => {
-  // express adds a "params" Object to requests
-  const id = request.params.id;
-  // handle GET request for post with an id of "id"
-  response.send(JSON.stringify({ user_id: id }));
-});
+// app.get("/users/:id", (request, response) => {
+//   // express adds a "params" Object to requests
+//   const id = request.params.id;
+//   // handle GET request for post with an id of "id"
+//   response.send(JSON.stringify({ user_id: id }));
+// });
 
-app.post("/add", (request, response) => {
-  const num1 = request.body.numberOne;
-  const num2 = request.body.numberTwo;
-  const responseBody = {
-    sum: num1 + num2,
-  };
-  response.json(responseBody);
-});
+// app.post("/add", (request, response) => {
+//   const num1 = request.body.numberOne;
+//   const num2 = request.body.numberTwo;
+//   const responseBody = {
+//     sum: num1 + num2,
+//   };
+//   response.json(responseBody);
+// });
+
+app.use("/pizzas", pizzas);
 
 // Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
