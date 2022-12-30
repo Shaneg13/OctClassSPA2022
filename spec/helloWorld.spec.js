80837993f2fe1fec6949fbbed8e11c77eca269fa
Shaneg13 <sgrewell713@gmail.com>
2022-12-29 20:15:14 -0500

    Class SPA commit 29 Dec 2022

---
+++ spec/helloWorld.spec.js
@@ -0,0 +1,15 @@
+const assert = require("assert");
+const helloWorld = require("../helloWorld");
+
+describe("helloWorld()", function() {
+  //Arrange
+  let greeting = "Hello World";
+
+  //Act
+  let result = helloWorld();
+
+  //Assert
+  it("it should return Hello World", function() {
+    expect(result).toEqual(greeting);
+  });
+});
